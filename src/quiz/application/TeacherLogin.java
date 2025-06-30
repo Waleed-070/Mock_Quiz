b57ad3ab55030128d7d3a268729c50f2dc1deaa2
Waleed-070 <w56772486@gmail.com>
2025-06-30 12:04:51 +0500

    pusihg teh changes

--- src/quiz/application/TeacherLogin.java
+++ src/quiz/application/TeacherLogin.java
@@ -14,74 +14,73 @@ public class TeacherLogin extends JFrame implements ActionListener {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
-        // Create a main panel with BorderLayout to center the login panel
-        JPanel mainPanel = new JPanel(new GridBagLayout());
-        mainPanel.setBackground(Color.WHITE);
+        // Add background image
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/teacherLogin.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(600, 500, Image.SCALE_SMOOTH);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 600, 500);
+        add(image);
         
+        // Login panel on the right side
         JPanel loginPanel = new JPanel();
         loginPanel.setLayout(null);
         loginPanel.setBackground(Color.WHITE);
-        loginPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
-        loginPanel.setPreferredSize(new Dimension(400, 350));
+        loginPanel.setBounds(600, 0, 600, 500);
+        add(loginPanel);
         
         JLabel heading = new JLabel("Teacher Login");
-        heading.setBounds(100, 20, 300, 45);
+        heading.setBounds(100, 60, 300, 45);
         heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 30));
         heading.setForeground(new Color(30, 144, 254));
         loginPanel.add(heading);
         
         JLabel email = new JLabel("Email");
-        email.setBounds(50, 80, 300, 20);
+        email.setBounds(50, 150, 300, 20);
         email.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
         email.setForeground(new Color(30, 144, 254));
         loginPanel.add(email);
         
         tfemail = new JTextField();
-        tfemail.setBounds(50, 110, 300, 25);
+        tfemail.setBounds(50, 180, 300, 25);
         tfemail.setFont(new Font("Times New Roman", Font.BOLD, 16));
         loginPanel.add(tfemail);
         
         JLabel password = new JLabel("Password");
-        password.setBounds(50, 150, 300, 20);
+        password.setBounds(50, 220, 300, 20);
         password.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
         password.setForeground(new Color(30, 144, 254));
         loginPanel.add(password);
         
         tfpassword = new JPasswordField();
-        tfpassword.setBounds(50, 180, 300, 25);
+        tfpassword.setBounds(50, 250, 300, 25);
         tfpassword.setFont(new Font("Times New Roman", Font.BOLD, 16));
         loginPanel.add(tfpassword);
         
         register = new JButton("Register");
-        register.setBounds(50, 230, 120, 30);
+        register.setBounds(50, 300, 120, 30);
         register.setBackground(new Color(30, 144, 254));
         register.setForeground(Color.WHITE);
         register.addActionListener(this);
         loginPanel.add(register);
         
         login = new JButton("Login");
-        login.setBounds(230, 230, 120, 30);
+        login.setBounds(230, 300, 120, 30);
         login.setBackground(new Color(30, 144, 254));
         login.setForeground(Color.WHITE);
         login.addActionListener(this);
         loginPanel.add(login);
         
         back = new JButton("Back");
-        back.setBounds(140, 280, 120, 30);
+        back.setBounds(140, 350, 120, 30);
         back.setBackground(new Color(30, 144, 254));
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         loginPanel.add(back);
         
-        // Add the login panel to the main panel which will center it
-        mainPanel.add(loginPanel);
-        
-        // Add the main panel to the frame
-        setContentPane(mainPanel);
-        
         // Set frame properties
-        setSize(600, 500);
-        setLocationRelativeTo(null); // Center the window on the screen
+        setSize(1200, 500);
+        setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
     }
